3cac5b122a895873beb8699a7c17146674f95655
t1r <>
2022-12-10 21:28:35 +0300

    wip

--- ui-compose/build.gradle.kts
+++ ui-compose/build.gradle.kts
@@ -7,25 +7,21 @@ kotlin {
     sourceSets {
         val commonMain by getting {
             dependencies {
-//                api(compose.runtime)
-//                api(compose.foundation)
                 implementation(compose.material)
                 implementation(Deps.Decompose.decompose)
                 implementation(Deps.Decompose.extensionsComposeJetbrains)
 
+                implementation(project(Module.entity))
                 implementation(project(Module.Feature.root))
                 implementation(project(Module.Feature.baselineColor))
             }
         }
         val androidMain by getting {
             dependencies {
-//                api("androidx.appcompat:appcompat:1.2.0")
-//                api("androidx.core:core-ktx:1.3.1")
             }
         }
         val desktopMain by getting {
             dependencies {
-//                api(compose.preview)
                 implementation(compose.desktop.currentOs)
             }
         }
