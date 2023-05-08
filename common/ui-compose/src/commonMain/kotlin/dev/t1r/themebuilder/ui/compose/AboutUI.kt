a49113f08215a3a6c7e73c5ae2127dcb6c00bc3f
t1r <>
2023-05-08 18:43:47 +0300

    android share WIP

--- common/ui-compose/src/commonMain/kotlin/dev/t1r/themebuilder/ui/compose/AboutUI.kt
+++ common/ui-compose/src/commonMain/kotlin/dev/t1r/themebuilder/ui/compose/AboutUI.kt
@@ -1,17 +1,23 @@
 package dev.t1r.themebuilder.ui.compose
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.ClickableText
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
 import dev.t1r.themebuilder.entity.navigation.DrawerNavigationModel
 import dev.t1r.themebuilder.ui.compose.common.ScreenContainerWidget
@@ -21,13 +27,14 @@ fun AboutContent(
     navigationModel: DrawerNavigationModel,
     modifier: Modifier = Modifier,
 ) {
+    val uriHandler = LocalUriHandler.current
     ScreenContainerWidget(
         navigationModel = navigationModel,
         title = "About",
         content = { pv ->
             Column(
                 modifier = Modifier
-                    .fillMaxWidth()
+                    .fillMaxSize()
                     .padding(pv)
                     .verticalScroll(rememberScrollState()),
             ) {
@@ -118,6 +125,22 @@ fun AboutContent(
                         fontFamily = FontFamily.Monospace,
                         color = Color.Black,
                     ),
+                )
+                Spacer(modifier = Modifier.weight(1F))
+                ClickableText(
+                    modifier = Modifier
+                        .padding(start = 16.dp, end = 16.dp, top = 6.dp, bottom = 12.dp)
+                        .fillMaxWidth(),
+                    text = buildAnnotatedString {
+                        append("Source code of Project on GitHub")
+                    },
+                    style = TextStyle(
+                        fontWeight = FontWeight.Medium,
+                        fontFamily = FontFamily.Monospace,
+                        color = Color.Black,
+                        textDecoration = TextDecoration.Underline
+                    ),
+                    onClick = { uriHandler.openUri("https://github.com/t1r/ComposeThemeBuilder") },
                 )
             }
         },
